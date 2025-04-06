cd5bf1310365fbd9202f90f20cb3059552e5dfc2
SamuelOchieng <o.samuelyannick@gmail.com>
2025-04-06 16:32:04 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -28,16 +28,38 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+            
+        return {}, 204
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+        user = User.query.filter(User.username == username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {'message': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
